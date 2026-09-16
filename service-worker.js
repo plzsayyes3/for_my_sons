54526f79b8abced490720567f807d96486e3cc2d
plzsayyes3 <102906997+plzsayyes3@users.noreply.github.com>
2026-09-16 12:56:40 +0900

    feat: cache launcher shell for standalone use

---
+++ service-worker.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = 'for-my-sons-v1';
+const SHELL = [
+  './',
+  './index.html',
+  './styles.css',
+  './app.js',
+  './apps.json',
+  './manifest.webmanifest',
+  './assets/for-my-sons-icon.svg',
+  './assets/kids-3d.svg',
+  './assets/minecraft-english.svg'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(SHELL)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => Promise.all(
+      keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+    ))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request).then((cached) => cached || caches.match('./index.html')))
+  );
+});
