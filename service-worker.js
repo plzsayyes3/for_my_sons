eeb500498da79b024f20d3b6e473987f0d35f69f
plzsayyes3 <102906997+plzsayyes3@users.noreply.github.com>
2026-09-22 12:30:26 +0900

    Refresh piano cache for sound selector

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'for-my-sons-v5';
+const CACHE_NAME = 'for-my-sons-v6';
 const SHELL = [
   './',
   './index.html',
@@ -6,8 +6,8 @@ const SHELL = [
   './app.js',
   './apps.json',
   './manifest.webmanifest',
-  './piano/?v=2',
-  './piano/index.html?v=2',
+  './piano/?v=3',
+  './piano/index.html?v=3',
   './assets/for-my-sons-icon.svg',
   './assets/kids-3d.svg',
   './assets/minecraft-english.svg',
