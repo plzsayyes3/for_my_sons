76466c81c11b5d2051dd5e66813f3e9c7b3ad907
plzsayyes3 <102906997+plzsayyes3@users.noreply.github.com>
2026-09-22 12:40:21 +0900

    Refresh piano cache for vertical synth faders

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'for-my-sons-v9';
+const CACHE_NAME = 'for-my-sons-v10';
 const SHELL = [
   './',
   './index.html',
@@ -6,8 +6,8 @@ const SHELL = [
   './app.js',
   './apps.json',
   './manifest.webmanifest',
-  './piano/?v=6',
-  './piano/index.html?v=6',
+  './piano/?v=7',
+  './piano/index.html?v=7',
   './assets/for-my-sons-icon.svg',
   './assets/kids-3d.svg',
   './assets/minecraft-english.svg',
