2eecce38ca501e4b33b751652b2c395a5a0b3a66
plzsayyes3 <102906997+plzsayyes3@users.noreply.github.com>
2026-09-22 12:32:18 +0900

    Refresh piano cache for melodica

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'for-my-sons-v7';
+const CACHE_NAME = 'for-my-sons-v8';
 const SHELL = [
   './',
   './index.html',
@@ -6,8 +6,8 @@ const SHELL = [
   './app.js',
   './apps.json',
   './manifest.webmanifest',
-  './piano/?v=4',
-  './piano/index.html?v=4',
+  './piano/?v=5',
+  './piano/index.html?v=5',
   './assets/for-my-sons-icon.svg',
   './assets/kids-3d.svg',
   './assets/minecraft-english.svg',
