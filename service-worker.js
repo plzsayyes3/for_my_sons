4cdeeec61f12917540e514861538fe72a0a6f005
plzsayyes3 <102906997+plzsayyes3@users.noreply.github.com>
2026-09-23 09:45:33 +0900

    Refresh cache for Wanko War iOS zoom fix

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'for-my-sons-v33';
+const CACHE_NAME = 'for-my-sons-v34';
 const SHELL = [
   './',
   './index.html',
@@ -16,7 +16,7 @@ const SHELL = [
   './merge-block/index.html',
   './merge-block/styles.css?v=10',
   './merge-block/app.js?v=11',
-  './wanko-war/?v=2',
+  './wanko-war/?v=3',
   './wanko-war/index.html',
   './paint/?v=3',
   './paint/index.html',
